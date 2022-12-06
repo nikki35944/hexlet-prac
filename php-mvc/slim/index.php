2329fd0692352d21fad608fcae3be941dd0ef9c9
nikki35944 <95740936+nikki35944@users.noreply.github.com>
2022-12-06 14:41:49 +0300

    handlers lesson

--- php-mvc/slim/index.php
+++ php-mvc/slim/index.php
@@ -5,6 +5,20 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 
+$faker = \Faker\Factory::create();
+$faker->seed(1234);
+
+$domains = [];
+for ($i = 0; $i < 10; $i++) {
+    $domains[] = $faker->domainName;
+}
+
+$phones = [];
+for ($i = 0; $i < 10; $i++) {
+    $phones[] = $faker->phoneNumber;
+}
+
+
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
@@ -14,4 +28,17 @@ $app->get('/', function ($request, $response) {
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
 });
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->get('/phones', function ($request, $response) use ($phones) {
+    return $response->write(json_encode($phones));
+});
+$app->get('/domains', function ($request, $response) use ($domains) {
+    return $response->write(json_encode($domains));
+});
+
+
 $app->run();
