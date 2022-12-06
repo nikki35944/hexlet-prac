e0d7ac78005f5859f53d90aa205534b66b86afa3
nikki35944 <95740936+nikki35944@users.noreply.github.com>
2022-12-06 18:39:24 +0300

    templates

--- php-mvc/slim/index.php
+++ php-mvc/slim/index.php
@@ -4,11 +4,11 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
+use Illuminate\Support\Collection;
 
 $faker = \Faker\Factory::create();
 $faker->seed(1234);
-
-
 
 $domains = [];
 for ($i = 0; $i < 10; $i++) {
@@ -19,18 +19,20 @@ for ($i = 0; $i < 10; $i++) {
     $phones[] = $faker->phoneNumber;
 }
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
-});
-
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
 });
 
 $app->get('/phones', function ($request, $response) use ($phones) {
@@ -40,13 +42,8 @@ $app->get('/domains', function ($request, $response) use ($domains) {
     return $response->write(json_encode($domains));
 });
 
-$app->post('/users', function ($request, $response) {
-    return $response->withStatus(302);
-});
-
 
 $companies = \App\Generator::generate(100);
-
 
 $app->get('/companies', function ($request, $response) use ($companies) {
     $page = $request->getQueryParam('page', 1);
@@ -58,7 +55,8 @@ $app->get('/companies', function ($request, $response) use ($companies) {
     return $response->write(json_encode($result));
 });
 
-use Illuminate\Support\Collection;
+
+
 
 $app->get('/companies/{id}', function ($request, $response, $args) use ($companies) {
     $collection = collect($companies);
@@ -68,5 +66,30 @@ $app->get('/companies/{id}', function ($request, $response, $args) use ($compani
     }
     return $response->write(json_encode($result));
 });
+
+
+// Список пользователей
+// Каждый пользователь – ассоциативный массив
+// следующей структуры: id, firstName, lastName, email
+$users = \App\UsersGenerator::generate(100);
+
+$app->get('/users', function ($request, $response) use ($users) {
+    $params = [
+        'users' => $users,
+    ];
+
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) use ($users) {
+    $id = (int) $args['id'];
+    $user = collect($users)->firstWhere('id', $id);
+    $params = [
+        'user' => $user
+    ];
+
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
 
 $app->run();
